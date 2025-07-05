93a6a086235dbebd42b5a046746909c6dd8ecfb3
Eyuzikfr <eyuzikchhunzu2@gmail.com>
2025-07-05 14:34:56 +0545

    update dictionary lab2a.py

--- lab2a.py
+++ lab2a.py
@@ -1,9 +1,13 @@
 graph = {
-    'A': ['B', 'C'],    # A is connected to B and C
-    'B': ['D', 'E'],    # B is connected to D and E
-    'C': ['F'],         # C is connected to F
-    'D': ['G'],         # D is connected to G
-    'E': ['H'],         # E is connected to H
+    'A': ['B', 'C'],         # A is connected to B and C
+    'B': ['A', 'D', 'E'],    # B is connected to D and E
+    'C': ['A', 'F'],         # C is connected to F
+    'D': ['B', 'G'],         # D is connected to G
+    'E': ['B', 'H'],         # E is connected to H
+    'F': ['C'],              # F is connected to C
+    'G': ['D'],              # G is connected to D
+    'H': ['E']
+
 }
 
 print(graph)
